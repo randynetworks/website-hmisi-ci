af0519c1ed96fbcbc1740755357b4518c97007e2
randynetworks <randynetworks@gmail.com>
2020-05-21 16:54:28 +0700

    menyelesaikan bagian registration

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,20 +7,69 @@ class Auth extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->library('form_validation');
     }
 
     public function index()
     {
 
-        $this->load->view('templates/auth_header');
+        $data['title'] = 'Login Page';
+
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/login');
         $this->load->view('templates/auth_footer');
     }
 
     public function registration()
     {
-        $this->load->view('templates/auth_header');
-        $this->load->view('auth/registration');
-        $this->load->view('templates/auth_footer');
+        // role
+        // roll fullname | gk boleh kosong
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+
+        // role email
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'Is Email already used'
+        ]);
+
+        // role pass1
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password dont match',
+            'min_length' => 'Password too short'
+        ]);
+
+        // role pass2
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password2]');
+
+
+
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Registration Page';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $data = [
+
+                'name'          => $this->input->post('name'),
+                'email'         => $this->input->post('email'),
+                'email'         => $this->input->post('email'),
+                'image'         => 'default.jpg',
+                'password'      => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id'       => 2,
+                'is_active'     => 1,
+                'date_created'  => time()
+
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata(
+                'message',
+                '<div class="alert alert-success" role="alert">
+                Your Account has been created, please login!
+            </div>'
+            );
+            redirect('auth');
+        }
     }
 }
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,10 +1,7 @@
 <div class="container">
-
     <!-- Outer Row -->
     <div class="row justify-content-center">
-
         <div class="col-lg-7">
-
             <div class="card o-hidden border-0 shadow-lg my-5">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
@@ -14,6 +11,7 @@
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Selamat Datang, Admin!</h1>
                                 </div>
+                                <?= $this->session->flashdata('message'); ?>
                                 <form class="user">
                                     <div class="form-group">
                                         <input type="text" class="form-control form-control-user" id="email" placeholder="Enter Email Address..." name="email">
@@ -38,9 +36,6 @@
                     </div>
                 </div>
             </div>
-
         </div>
-
     </div>
-
 </div>
