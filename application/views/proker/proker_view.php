49692bd13831c75f3be960ee78353e1ec388d8e2
randynetworks <randynetworks@gmail.com>
2020-05-28 14:16:10 +0700

    fix proker view

--- application/views/proker/proker_view.php
+++ application/views/proker/proker_view.php
@@ -9,20 +9,22 @@
 
 		<section id="portfolio" class="section-bg">
 			<div class="container">
-				<? foreach ($imgproker as $img) : ?>
-				<div class="col-lg-4 col-md-6 portfolio-item filter-app">
-					<div class="portfolio-wrap">
-						<img src="<?= base_url('assets/img/proker-img/') . $img['img']; ?>" class="img-fluid" alt="">
-						<div class="portfolio-info">
-							<h4><a href="#"><?= $img['text']; ?></a></h4>
-							<div>
-								<a href="<?= base_url('assets/img/proker-img/') . $img['img']; ?>" data-lightbox="portfolio" data-title="<?= $img['text']; ?>" class="link-preview" title="Preview"><i class="ion ion-eye"></i></a>
+				<div class="row text-center">
+					<? foreach ($imgproker as $img) : ?>
+					<div class="col-lg-3 col-md-4 col-6 portfolio-item filter-app">
+						<div class="portfolio-wrap">
+							<img src="<?= base_url('assets/img/proker-img/') . $img['img']; ?>" class="img-fluid" alt="">
+							<div class="portfolio-info">
+								<h6><a href="#"><?= $img['text']; ?></a></h6>
+								<div>
+									<a href="<?= base_url('assets/img/proker-img/') . $img['img']; ?>" data-lightbox="portfolio" class="link-preview" title="Preview"><i class="ion ion-eye"></i></a>
+								</div>
 							</div>
 						</div>
 					</div>
+					<? endforeach; ?>
+
 				</div>
-				<? endforeach; ?>
-			</div>
 		</section>
 	</div>
 </section>
