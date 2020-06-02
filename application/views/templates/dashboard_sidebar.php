8c4ba0e2d283e32f2772adb8b3432731a4568017
randynetworks <randynetworks@gmail.com>
2020-06-02 13:50:15 +0700

    fix sidebar

--- application/views/templates/dashboard_sidebar.php
+++ application/views/templates/dashboard_sidebar.php
@@ -1,93 +1,93 @@
 <!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-code"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">HMISI Dashboard</div>
-    </a>
+	<!-- Sidebar - Brand -->
+	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= base_url('dashboard') ?>">
+		<div class="sidebar-brand-icon rotate-n-15">
+			<i class="fas fa-code"></i>
+		</div>
+		<div class="sidebar-brand-text mx-3">HMISI Dashboard</div>
+	</a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+	<!-- Divider -->
+	<hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Administrator
-    </div>
+	<!-- Heading -->
+	<div class="sidebar-heading">
+		Administrator
+	</div>
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('dashboard') ?>">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+	<!-- Nav Item - Dashboard -->
+	<li class="nav-item">
+		<a class="nav-link" href="<?= base_url('dashboard') ?>">
+			<i class="fas fa-fw fa-tachometer-alt"></i>
+			<span>Dashboard</span></a>
+	</li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+	<!-- Divider -->
+	<hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Admin Management
-    </div>
-    <!-- Nav Item - list admin account -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('Admin_Profile'); ?>">
-            <i class="fas fa-fw fa-user"></i>
-            <span>Admin Profile</span></a>
-    </li>
+	<!-- Heading -->
+	<div class="sidebar-heading">
+		Admin Management
+	</div>
+	<!-- Nav Item - list admin account -->
+	<li class="nav-item">
+		<a class="nav-link" href="<?= base_url('Admin_Profile'); ?>">
+			<i class="fas fa-fw fa-user"></i>
+			<span>Admin Profile</span></a>
+	</li>
 
-    <!-- Nav Item - list admin account -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('Admin_List'); ?>">
-            <i class="fas fa-fw fa-users-cog"></i>
-            <span>List Admin Account</span></a>
-    </li>
+	<!-- Nav Item - list admin account -->
+	<li class="nav-item">
+		<a class="nav-link" href="<?= base_url('Admin_List'); ?>">
+			<i class="fas fa-fw fa-users-cog"></i>
+			<span>List Admin Account</span></a>
+	</li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
+	<!-- Divider -->
+	<hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Input Data
-    </div>
+	<!-- Heading -->
+	<div class="sidebar-heading">
+		Input Data
+	</div>
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link" href="" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span>
-        </a>
-        <div id="collapsePages" class="collapse hide" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Anggota HMISI:</h6>
-                <a class="collapse-item" href="<?= base_url('Input_Member'); ?>">Member HMISI</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Input:</h6>
-                <a class="collapse-item" href="<?= base_url('Input_Proker'); ?>">Input Proker</a>
-                <a class="collapse-item" href="<?= base_url('Input_Berita'); ?>">Input Berita</a>
-            </div>
-        </div>
-    </li>
+	<!-- Nav Item - Pages Collapse Menu -->
+	<li class="nav-item">
+		<a class="nav-link" href="" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+			<i class="fas fa-fw fa-table"></i>
+			<span>Tables</span>
+		</a>
+		<div id="collapsePages" class="collapse hide" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+			<div class="bg-white py-2 collapse-inner rounded">
+				<h6 class="collapse-header">Anggota HMISI:</h6>
+				<a class="collapse-item" href="<?= base_url('Input_Member'); ?>">Member HMISI</a>
+				<div class="collapse-divider"></div>
+				<h6 class="collapse-header">Other Input:</h6>
+				<a class="collapse-item" href="<?= base_url('Input_Proker'); ?>">Input Proker</a>
+				<a class="collapse-item" href="<?= base_url('Input_Berita'); ?>">Input Berita</a>
+			</div>
+		</div>
+	</li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+	<!-- Divider -->
+	<hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Nav Item - logout account -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('auth/logout') ?>" data-toggle="modal" data-target="#logoutModal">
-            <i class="fas fa-sign-out-alt"></i>
-            <span>logout</span></a>
-    </li>
+	<!-- Nav Item - logout account -->
+	<li class="nav-item">
+		<a class="nav-link" href="<?= base_url('auth/logout') ?>" data-toggle="modal" data-target="#logoutModal">
+			<i class="fas fa-sign-out-alt"></i>
+			<span>logout</span></a>
+	</li>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+	<!-- Divider -->
+	<hr class="sidebar-divider d-none d-md-block">
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+	<!-- Sidebar Toggler (Sidebar) -->
+	<div class="text-center d-none d-md-inline">
+		<button class="rounded-circle border-0" id="sidebarToggle"></button>
+	</div>
 
 </ul>
 <!-- End of Sidebar -->
